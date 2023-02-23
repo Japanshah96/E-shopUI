821765cba38528c49407392aae199a6ca80b7fea
Japan Shah <190630107200@mbit.edu.in>
2023-02-23 16:11:34 +0530

    animated bottom navigation bar

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -19,50 +20,102 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       bottomNavigationBar:
           //  BottomAppBar(child: ,),
+          // NavigationBarTheme(
+          //   data: NavigationBarThemeData(indicatorColor: Colors.blue.shade100),
+          // ),
+
           Obx(
-        () => BottomNavigationBar(
-            onTap: (value) {
-              controller.tabidex.value = value;
-            },
-            type: BottomNavigationBarType.fixed,
-            backgroundColor: Colors.blue.withOpacity(0.3),
-            // selectedItemColor: Colors.blue[700],
-            // unselectedItemColor: Colors.black,
-            items: [
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.home_rounded,
-                  color: controller.tabidex.value == 0
-                      ? Colors.blueAccent
-                      : Colors.black,
-                ),
-                label: 'home',
-              ),
-              BottomNavigationBarItem(
-                  icon: Icon(
-                    CupertinoIcons.heart_fill,
-                    color: controller.tabidex.value == 1
-                        ? Colors.blueAccent
-                        : Colors.black,
-                  ),
-                  label: 'favourites'),
-              BottomNavigationBarItem(
-                  icon: Icon(
-                    Icons.shopping_cart,
-                    color: controller.tabidex.value == 2
-                        ? Colors.blueAccent
-                        : Colors.black,
-                  ),
-                  label: 'cart'),
-              BottomNavigationBarItem(
-                  icon: Icon(
-                    Icons.person,
-                    color: controller.tabidex.value == 3
-                        ? Colors.blueAccent
-                        : Colors.black,
-                  ),
-                  label: 'profile'),
-            ]),
+        () => GNav(
+          gap: 8,
+          backgroundColor: Colors.blue.withOpacity(0.3),
+          color: Colors.black,
+          activeColor: Colors.black,
+          tabBackgroundColor: Colors.grey.withOpacity(0.3),
+          padding: EdgeInsets.all(18),
+          tabActiveBorder: Border(bottom: BorderSide.none),
+          onTabChange: (value) {
+            controller.tabidex.value = value;
+          },
+          tabs: [
+            GButton(
+              iconColor:
+                  controller.tabidex.value == 0 ? Colors.black : Colors.black,
+              icon: Icons.home_rounded,
+              // onPressed: (value) {
+              //   controller.tabidex.value = value;
+              // },
+              text: 'Home',
+            ),
+            GButton(
+                iconColor:
+                    controller.tabidex.value == 1 ? Colors.black : Colors.black,
+                icon: Icons.favorite,
+                // onPressed: (value) {
+                //   controller.tabidex.value = value;
+                // },
+                text: 'Favourites'),
+            GButton(
+                iconColor:
+                    controller.tabidex.value == 2 ? Colors.black : Colors.black,
+                icon: Icons.shopping_cart,
+                // onPressed: (value) {
+                //   controller.tabidex.value = value;
+                // },
+                text: 'Add to Cart'),
+            GButton(
+                iconColor:
+                    controller.tabidex.value == 3 ? Colors.black : Colors.black,
+                icon: Icons.person,
+                // onPressed: (value) {
+                //   controller.tabidex.value = value;
+                // },
+                text: 'Profile'),
+          ],
+        ),
+
+        //  BottomNavigationBar(
+        //     onTap: (value) {
+        //       controller.tabidex.value = value;
+        //     },
+        //     type: BottomNavigationBarType.fixed,
+        //     backgroundColor: Colors.blue.withOpacity(0.3),
+        //     // selectedItemColor: Colors.blue[700],
+        //     // unselectedItemColor: Colors.black,
+        //     items: [
+        //       BottomNavigationBarItem(
+        //         icon: Icon(
+        //           Icons.home_rounded,
+        //           color: controller.tabidex.value == 0
+        //               ? Colors.blueAccent
+        //               : Colors.black,
+        //         ),
+        //         label: 'home',
+        //       ),
+        //       BottomNavigationBarItem(
+        //           icon: Icon(
+        //             CupertinoIcons.heart_fill,
+        //             color: controller.tabidex.value == 1
+        //                 ? Colors.blueAccent
+        //                 : Colors.black,
+        //           ),
+        //           label: 'favourites'),
+        //       BottomNavigationBarItem(
+        //           icon: Icon(
+        //             Icons.shopping_cart,
+        //             color: controller.tabidex.value == 2
+        //                 ? Colors.blueAccent
+        //                 : Colors.black,
+        //           ),
+        //           label: 'cart'),
+        //       BottomNavigationBarItem(
+        //           icon: Icon(
+        //             Icons.person,
+        //             color: controller.tabidex.value == 3
+        //                 ? Colors.blueAccent
+        //                 : Colors.black,
+        //           ),
+        //           label: 'profile'),
+        //     ]),
       ),
       body: Obx(
         () => IndexedStack(
